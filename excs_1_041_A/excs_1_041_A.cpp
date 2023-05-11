90fa07045d84e9bb28d9f090023ef4a41dbb013c
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-11 10:40:07 +0700

    Add project files.

---
+++ excs_1_041_A/excs_1_041_A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+{def binary_search(arr, desired)
+    lowerbound = 0
+    upperbound = int(ihsan) - 1
+    while lowerbound <= upperbound :
+        mid = (lowerbound + upperbound) / 2
+        if ihsan[mid] == desired :
+            display("Found")
+
+
+int ihsan(37);
+{
+        if arr[mid] > desired:
+        arr = [1, 3, 5, 7, 9]
+            desired = 5
+            upperbound = mid - 1
+        else;
+        lowerbound = mid + 1
+        cout << endl;
+        display("Not Found");
+        return 0;
+}
